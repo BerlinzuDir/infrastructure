6f8345c64471ccf16404160a94ba9cd913200079
kilian <kilian.zimmerer@gmail.com>
2022-04-11 18:38:03 +0200

    fix stysle

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -16,20 +16,20 @@ resource aws_s3_bucket decilo_core_api {
 
 data aws_s3_object decilo_core_api {
   bucket = aws_s3_bucket.decilo_core_api.bucket
-  key = "artifact.zip"
+  key    = "artifact.zip"
 }
 
 resource "aws_lambda_function" "decilo_core_api" {
   function_name = "decilo_core_api"
-  s3_bucket = aws_s3_bucket.decilo_core_api.id
-  s3_key    = data.aws_s3_object.decilo_core_api.key
-  runtime = "python3.8"
-  handler = "decilo_core.main.handler"
-  role = aws_iam_role.lambda_exec.arn
+  s3_bucket     = aws_s3_bucket.decilo_core_api.id
+  s3_key        = data.aws_s3_object.decilo_core_api.key
+  runtime       = "python3.8"
+  handler       = "decilo_core.main.handler"
+  role          = aws_iam_role.lambda_exec.arn
 }
 
 resource "aws_cloudwatch_log_group" "decilo_core_api" {
-  name = "/aws/lambda/${aws_lambda_function.decilo_core_api.function_name}"
+  name              = "/aws/lambda/${aws_lambda_function.decilo_core_api.function_name}"
   retention_in_days = 30
 }
 
@@ -37,14 +37,15 @@ resource "aws_iam_role" "lambda_exec" {
   name = "serverless_lambda"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       }
     ]
   })
